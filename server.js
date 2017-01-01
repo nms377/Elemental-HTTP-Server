0df747e893d2789a1d212d4aba761a68845e74b1
nms377 <nms377@nyu.edu>
2016-12-31 16:38:47 -1000

    Created http module setup. Created html files to test my GET and it works. Now going to work on POST.

---
+++ server.js
@@ -0,0 +1,56 @@
+//jshint esversion: 6
+
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 8080;
+
+const fileNotFoundErrorHandler = (res) => {
+  res.statusCode = 500;
+  res.end('Server is broken');
+};
+
+const sendContent = (res, content) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write(content);
+  res.end();
+};
+
+const resourceMapping = {
+  '/styles' : './public/css/styles.css',
+  '/helium' : './public/helium.html',
+  '/hydrogen' : './public/hydrogen.html',
+  '/' : './public/index.html'
+};
+
+const server = http.createServer( (req, res) => {
+	console.log("reqURL", req.url);
+	console.log("req.method", req.method);
+	console.log("req.headers", req.headers);
+
+	let reqBody = '';
+	req.setEncoding('utf8');
+	req.on('data', (chunk) => {
+		reqBody += chunk;
+	});
+
+if(resourceMapping.hasOwnProperty(req.url) ){
+	fs.readFile(resourceMapping[req.url] || '', (err, content) =>{
+		if(err){
+			res.statusCode = 404;
+			res.write('Resource not found');
+			return
+		}
+
+		sendContent(res, content);
+	});
+} else {
+	res.statusCode = 404;
+	sendContent(res, 'Resource not found');
+	return;
+}
+
+});
+
+server.listen(PORT, () => {
+	console.log('server is listening on port', PORT);
+});
